736b46ebb7a9612bf7858f22acc982feee1d2b7b
OhKanghoon <ggaa96@naver.com>
2019-06-29 16:37:55 +0900

    modify dev info

---
+++ allnight-ios/Souces/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  allnight-ios
+//
+//  Created by ohkanghoon on 29/06/2019.
+//  Copyright © 2019 kanghoon. All rights reserved.
+//
+
+import UIKit
+
+UIApplicationMain(
+  CommandLine.argc,
+  CommandLine.unsafeArgv,
+  NSStringFromClass(UIApplication.self),
+  NSStringFromClass(NSClassFromString("allnight-iosTests.StubAppDelegate") ?? AppDelegate.self)
+)
---
+++ allnight-iosTests/Sources/StubAppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  StubAppDelegate.swift
+//  allnight-iosTests
+//
+//  Created by ohkanghoon on 29/06/2019.
+//  Copyright © 2019 kanghoon. All rights reserved.
+//
+
+import UIKit
+
+final class StubAppDelegate: UIResponder, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+    return true
+  }
+}
