0b72ee4c4dec1d01de72be29d9abe85651436308
OhKanghoon <ggaa96@naver.com>
2019-06-29 20:50:27 +0900

    폴더링용 모델 추가

---
+++ allnight-ios/Souces/Models/Recipe.swift
@@ -0,0 +1,13 @@
+//
+//  Recipe.swift
+//  allnight-ios
+//
+//  Created by ohkanghoon on 29/06/2019.
+//  Copyright © 2019 kanghoon. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+
+}
