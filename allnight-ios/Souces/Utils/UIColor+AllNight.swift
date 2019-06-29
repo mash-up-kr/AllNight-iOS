777d4c76e6195ccf58578c0d72300846ac0d5132
OhKanghoon <ggaa96@naver.com>
2019-06-30 01:43:32 +0900

    color scale 추가

---
+++ allnight-ios/Souces/Utils/UIColor+AllNight.swift
@@ -0,0 +1,35 @@
+//
+//  UIColor+AllNight.swift
+//  allnight-ios
+//
+//  Created by ohkanghoon on 30/06/2019.
+//  Copyright © 2019 kanghoon. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  class var anGray1: UIColor {
+    return UIColor(white: 216.0 / 255.0, alpha: 1.0)
+  }
+
+  class var anGray2: UIColor {
+    return UIColor(white: 183.0 / 255.0, alpha: 1.0)
+  }
+
+  class var anGray3: UIColor {
+    return UIColor(white: 75.0 / 255.0, alpha: 1.0)
+  }
+
+  class var anBlack1: UIColor {
+    return UIColor(red: 2.0 / 255.0, green: 3.0 / 255.0, blue: 8.0 / 255.0, alpha: 1.0)
+  }
+
+  class var anBlack2: UIColor {
+    return UIColor(red: 11.0 / 255.0, green: 12.0 / 255.0, blue: 21.0 / 255.0, alpha: 1.0)
+  }
+
+  class var anBrown: UIColor {
+    return UIColor(red: 211.0 / 255.0, green: 186.0 / 255.0, blue: 145.0 / 255.0, alpha: 1.0)
+  }
+}
