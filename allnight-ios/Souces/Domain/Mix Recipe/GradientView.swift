9f5ee4747d555639e7a04031d92e86ae7f7f643a
Kor-Chipmunk <rhj4862@gmail.com>
2019-08-23 17:49:38 +0900

    믹스 레시피 뷰 수정 [ 한 것 ] 1. 그라데이션 뷰 추가 2. 킹피셔 라이브러리 적용 3. 스크랩 기능 추가 4. RecipeCollectionViewCell 리팩토링 5. Recipe View 이동 추가
    [ 할 것 ]
    1. 칵테일 재료로 목록 불러오기
    2. 필터링 정보로 다시 칵테일 목록 불러오기
    
    더미데이터로 테스트 했습니다.
    모야 라이브러리 문제로 칵테일 목록이 불러오지 않습니다.
    다른 방법을 모색해야할 것 같습니다.

--- allnight-ios/Souces/Domain/Mix Recipe/GradientView.swift
+++ allnight-ios/Souces/Domain/Mix Recipe/GradientView.swift
@@ -1,21 +1,39 @@
-//
-//  GradientView.swift
-//  allnight-ios
-//
-//  Created by 다람쥐 on 23/08/2019.
-//  Copyright © 2019 kanghoon. All rights reserved.
-//
-
 import UIKit
 
+@IBDesignable
 class GradientView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+  
+  @IBInspectable var startColor:   UIColor = .black { didSet { updateColors() }}
+  @IBInspectable var endColor:     UIColor = .white { didSet { updateColors() }}
+  @IBInspectable var startLocation: Double =   0.05 { didSet { updateLocations() }}
+  @IBInspectable var endLocation:   Double =   0.95 { didSet { updateLocations() }}
+  @IBInspectable var horizontalMode:  Bool =  false { didSet { updatePoints() }}
+  @IBInspectable var diagonalMode:    Bool =  false { didSet { updatePoints() }}
+  
+  override public class var layerClass: AnyClass { return CAGradientLayer.self }
+  
+  var gradientLayer: CAGradientLayer { return layer as! CAGradientLayer }
+  
+  func updatePoints() {
+    if horizontalMode {
+      gradientLayer.startPoint = diagonalMode ? CGPoint(x: 1, y: 0) : CGPoint(x: 0, y: 0.5)
+      gradientLayer.endPoint   = diagonalMode ? CGPoint(x: 0, y: 1) : CGPoint(x: 1, y: 0.5)
+    } else {
+      gradientLayer.startPoint = diagonalMode ? CGPoint(x: 0, y: 0) : CGPoint(x: 0.5, y: 0)
+      gradientLayer.endPoint   = diagonalMode ? CGPoint(x: 1, y: 1) : CGPoint(x: 0.5, y: 1)
     }
-    */
-
+  }
+  func updateLocations() {
+    gradientLayer.locations = [startLocation as NSNumber, endLocation as NSNumber]
+  }
+  func updateColors() {
+    gradientLayer.colors    = [startColor.cgColor, endColor.cgColor]
+  }
+  
+  override public func layoutSubviews() {
+    super.layoutSubviews()
+    updatePoints()
+    updateLocations()
+    updateColors()
+  }
 }
