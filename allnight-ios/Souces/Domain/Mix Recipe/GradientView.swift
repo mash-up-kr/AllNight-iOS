c15a1bb4873585c954ae6e1193cbaf53f3cc43aa
Kor-Chipmunk <rhj4862@gmail.com>
2019-08-23 17:46:40 +0900

    AllNightAPI.swift 인덴테이션 수정

---
+++ allnight-ios/Souces/Domain/Mix Recipe/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  allnight-ios
+//
+//  Created by 다람쥐 on 23/08/2019.
+//  Copyright © 2019 kanghoon. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
