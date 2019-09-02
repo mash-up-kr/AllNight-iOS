6604d8af8d2cf9e221761543224ffa1e24ca7ece
Suji Kim <rlatnwl9595@naver.com>
2019-09-01 20:03:28 -0700

    시스템 언어에 따른 레시피 내용 설정

---
+++ allnight-ios/Souces/Domain/Recipe/LanguageManger.swift
@@ -0,0 +1,13 @@
+//
+//  Language.swift
+//  allnight-ios
+//
+//  Created by Suji Kim on 9/1/19.
+//  Copyright © 2019 kanghoon. All rights reserved.
+//
+
+import Foundation
+
+class LanguageManger {
+    static let language = Locale.current.languageCode ?? "en"
+}
